aceb0d21f63d4719c8b994b9118a0dfb0a4dce30
betharia.pn <betharia.pn@students.ac.id>
2021-03-16 20:44:30 +0700

    Tugas Taxi

---
+++ Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_taxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            if (OnDuty != true)
+                Console.WriteLine("On Duty : No");
+            else
+                Console.WriteLine("On Duty : Yes");
+        }
+
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang\n", DriverName);
+        }
+
+        public void PickOffPassanger()
+        {
+            Console.WriteLine("{0} selesai menjemput penumpang\n", DriverName);
+        }
+
+    }
+}
